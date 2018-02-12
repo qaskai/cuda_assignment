7212c0ea7db85150c39c2b8d99ff7ef33713cd13
Filip Bartodziej <filipbartodziej@gmail.com>
2018-02-12 19:26:22 +0100

    fixed segfault

--- cuda/forest/demo.cpp
+++ cuda/forest/demo.cpp
@@ -45,7 +45,6 @@ int main() {
     if (cuMemAlloc(&dev_parents, N * sizeof(int)) != CUDA_SUCCESS) { printf("cuMemAlloc dev_parents fail\n"); exit(-1);}
     
     gen_forest(trees_number, parents_table);
-
     if (cuMemcpyHtoD(dev_parents, parents_table, N* sizeof(int)) != CUDA_SUCCESS) { printf("cuMemcpyHtoD dev_parents\n"); exit(-1);}
     if (cuCtxSynchronize() != CUDA_SUCCESS) { printf("sync fail\n"); exit(-1);}
 
@@ -60,6 +59,5 @@ int main() {
     cuMemFree(dev_parents);
     cuMemFreeHost(parents_table);
     cuCtxDestroy(context);
-    free(parents_table);
-
+    printf("OK :)\n");
 }
